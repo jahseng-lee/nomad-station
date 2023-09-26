b9116c787f254c1311049b3cde92820a31e3c195
Jah Seng Lee <jahseng.lee@gmail.com>
2023-09-26 21:39:30 +0700

    Show description on search results

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,7 +1,13 @@
 module LocationsHelper
+  def helper_location_short_description(location:)
+    description = helper_location_description(location: location)
+
+    description.length <= 150 ? description : description[0..147] + "..."
+  end
+
   def helper_location_description(location:)
     if location.description.present?
-      location.description
+      simple_format(location.description)
     else
       "Make sure you put #{location.name_utf8} on your travel list!"
     end
