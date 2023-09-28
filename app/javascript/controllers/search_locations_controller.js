d829b3c8dd5c50ff9a388c9dfc56dd0061f5192b
Jah Seng Lee <jahseng.lee@gmail.com>
2023-09-28 15:15:27 +0700

    Ensure country select is cleared when region is re-selected

--- app/javascript/controllers/search_locations_controller.js
+++ app/javascript/controllers/search_locations_controller.js
@@ -2,8 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [
+    "countrySelect",
+    "regionSelect",
     "searchForm",
   ];
+
+  regionSelect() {
+    console.log(this.countrySelectTarget);
+    this.countrySelectTarget.value = "";
+    this.submitSearch();
+  }
 
   submitSearch() {
     this.searchFormTarget.submit();
