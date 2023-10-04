a0b27b999c284bf2a8d43cf49bf8c315b120dd46
Jah Seng Lee <jahseng.lee@gmail.com>
2023-10-04 15:48:00 +0700

    Add index to "name" for "locations"

---
+++ db/migrate/20231004084615_add_name_index_to_locations.rb
@@ -0,0 +1,5 @@
+class AddNameIndexToLocations < ActiveRecord::Migration[7.0]
+  def change
+    add_index :locations, :name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_01_073206) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_084615) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_01_073206) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.index ["country_id"], name: "index_locations_on_country_id"
+    t.index ["name"], name: "index_locations_on_name"
   end
 
   create_table "regions", force: :cascade do |t|
