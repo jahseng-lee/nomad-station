b37d7a8c90ee49870cff96352a7a7ffbbeba9212
Jah Seng Lee <jahseng.lee@gmail.com>
2023-10-14 03:10:16 +0700

    Implement updating password

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,5 +4,9 @@ class RegistrationsController < Devise::RegistrationsController
   def after_inactive_sign_up_path_for(*)
     new_user_session_path
   end
+
+  def after_update_path_for(resource)
+    profile_path
+  end
 end
 
