d7745cff2fd1f3c4a197eaf637f42382c52f0fa3
Jah Seng Lee <jahseng.lee@gmail.com>
2023-10-19 14:12:20 +0700

    Handle missing ENV vars gracefully

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -6,23 +6,27 @@ class ChatsController < ApplicationController
   before_action :authenticate_subscription!
 
   def show
-    if current_user.stream_user_id.nil?
-      @stream_user_id = SecureRandom.hex(16)
-      @stream_user_token = StreamChatClient.create_stream_user(
-        id: @stream_user_id
-      )
+    if (ENV["STREAM_API_KEY"] && ENV["STREAM_API_SECRET"]).present?
+      if current_user.stream_user_id.nil?
+        @stream_user_id = SecureRandom.hex(16)
+        @stream_user_token = StreamChatClient.create_stream_user(
+          id: @stream_user_id
+        )
 
-      # TODO error handling
-      current_user.update!(
-        stream_user_id: @stream_user_id,
-        stream_user_token: @stream_user_token
-      )
+        # TODO error handling
+        current_user.update!(
+          stream_user_id: @stream_user_id,
+          stream_user_token: @stream_user_token
+        )
+      else
+        @stream_user_id = current_user.stream_user_id
+        @stream_user_token = current_user.stream_user_token
+      end
+
+      add_current_user_to_channels
     else
-      @stream_user_id = current_user.stream_user_id
-      @stream_user_token = current_user.stream_user_token
+      @stream_env_vars_missing = true
     end
-
-    add_current_user_to_channels
   end
 
   private
