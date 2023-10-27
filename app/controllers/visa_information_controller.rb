57da30b9b1650b89696d3d19e4ae2ec305f9c205
Jah Seng Lee <jahseng.lee@gmail.com>
2023-10-27 23:23:43 +0800

    Use shared application markdown renderer

--- app/controllers/visa_information_controller.rb
+++ app/controllers/visa_information_controller.rb
@@ -1,11 +1,9 @@
 class VisaInformationController < ApplicationController
+  before_action :initialize_markdown_renderer, only: :show
+
   def show
     @location = Location.find(params[:location_id])
     @country = @location.country
-
-    # TODO move this renderer to application_controller? Or new specialty controller?
-    renderer = Redcarpet::Render::HTML.new(render_options = {})
-    @markdown = Redcarpet::Markdown.new(renderer, extensions = {})
   end
 
   def edit
