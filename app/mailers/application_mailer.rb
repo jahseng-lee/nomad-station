68e2ea2630fae0b8efa19471673e3a8bc3a78e7a
Jah Seng Lee <jahseng.lee@gmail.com>
2023-10-28 16:39:25 +0800

    Set default_url_options in staging conditionally

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,15 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
   layout "mailer"
+
+  def default_url_options
+    # In heroku, Rails.env.production? is true in
+    # staging and production, so custom set an ENV var
+    if ENV["RAILS_ENV"] == "staging"
+      { host: "https://staging.nomadstation.io" }
+    else
+      # Use default
+      Rails.application.config.action_mailer.default_url_options
+    end
+  end
 end
