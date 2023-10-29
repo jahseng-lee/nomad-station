06ae3e161dbc8899b88e9d6f37814d4e3a420b7c
Jah Seng Lee <jahseng.lee@gmail.com>
2023-10-29 16:55:48 +0800

    Remove existing chat code

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -5,51 +5,5 @@ class ChatsController < ApplicationController
 
   before_action :authenticate_subscription!
 
-  def show
-    if (ENV["STREAM_API_KEY"] && ENV["STREAM_API_SECRET"]).present?
-      if current_user.stream_user_id.nil?
-        @stream_user_id = SecureRandom.hex(16)
-        @stream_user_token = StreamChatClient.create_stream_user(
-          id: @stream_user_id
-        )
-
-        # TODO error handling
-        current_user.update!(
-          stream_user_id: @stream_user_id,
-          stream_user_token: @stream_user_token
-        )
-      else
-        @stream_user_id = current_user.stream_user_id
-        @stream_user_token = current_user.stream_user_token
-      end
-
-      add_current_user_to_channels
-    else
-      @stream_env_vars_missing = true
-    end
-  end
-
-  private
-
-  def add_current_user_to_channels
-    [
-      { type: "messaging", id: "general" },
-      { type: "messaging", id: "feedback-and-requests" },
-      { type: "messaging", id: "bugs" },
-    ].each do |channel|
-      channel = StreamChatClient.get_channel(
-        type: channel[:type],
-        channel_id: channel[:id],
-      )
-      unless StreamChatClient.channel_include?(
-          channel: channel,
-          user_id: current_user.stream_user_id
-      )
-        StreamChatClient.add_member(
-          channel: channel,
-          user_id: current_user.stream_user_id
-        )
-      end
-    end
-  end
+  def show; end
 end
