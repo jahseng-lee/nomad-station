71cfffb9e7b372423bfc7ccb533bc15f89cbbd52
Jah Seng Lee <jahseng.lee@gmail.com>
2023-11-24 17:06:47 +0800

    Use placeholder profile picture in chat

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,12 +1,2 @@
 module ProfilesHelper
-  def helper_user_initials(user:)
-    split_display_name = user.display_name.upcase.split(" ")
-    if split_display_name.length >= 2
-      "#{split_display_name[0][0]}#{split_display_name[1][0]}"
-    elsif split_display_name[0].length >= 2
-      "#{split_display_name[0][0]}#{split_display_name[0][1]}"
-    else
-      "#{split_display_name[0]}"
-    end
-  end
 end
