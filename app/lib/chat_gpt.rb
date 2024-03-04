453a85f9b20a89e46c0cdb8dcf613e794f61c445
Jah Seng Lee <jahseng.lee@gmail.com>
2024-03-04 15:55:40 +0900

    Remove unused method

--- app/lib/chat_gpt.rb
+++ app/lib/chat_gpt.rb
@@ -20,27 +20,4 @@ class ChatGpt
     )
     response.dig("choices")[0].dig("message", "content")
   end
-
-  def self.generate_location_review(location:)
-    client = OpenAI::Client.new
-    response = client.chat(
-      parameters: {
-        model: "gpt-3.5-turbo",
-        messages: [
-          {
-            role: "user",
-            content: "I want a review of"\
-            " #{location.name_utf8}, #{location.country.name}. I want you"\
-            " to rate it on a scale of 1-5, for the following things:"\
-            " overall, fun, internet, cost, safety. I want the response"\
-            " in a JSON format, i.e. { overall: x, fun: x, cost: x,"\
-            " internet: x, safety: x }. I want each rating to be an"\
-            " integer."
-          }
-        ],
-        temperature: 0.7,
-      }
-    )
-   JSON.parse(response.dig("choices")[0].dig("message", "content"))
-  end
 end
