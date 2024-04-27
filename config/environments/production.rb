f5bb40670426fa030aed6316ee21f1f73f4d6dd2
Jah Seng Lee <jahseng.lee@gmail.com>
2024-04-27 16:34:30 +0900

    Try something else dumb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  config.action_cable.url = "wss://nomad-station-production-4284b123c815.herokuapp.com/cable"
+  config.action_cable.url = "wss://round-tomato-aaucx01kk0pnqsa92tssrxd7.herokudns.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
