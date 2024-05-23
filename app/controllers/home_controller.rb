3a143eb148c65e46d9f6d5c0ab938cf40f9f0dff
Jah Seng Lee <jahseng.lee@gmail.com>
2024-05-24 08:38:42 +0900

    Preload associations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,9 @@ class HomeController < ApplicationController
     @pagy, @locations = pagy(
       Location.all
         .includes(:tags)
+        .includes(:banner_image)
+        .includes(:country)
+        .includes(:reviews)
         .ordered_for_search_results,
       items: 18
     )
