580327a5aaf7a1610ad2f075b3d02d4c483ce1c7
Jah Seng Lee <jahseng.lee@gmail.com>
2024-05-24 10:48:53 +0900

    Add format converter to webp

--- app/uploaders/banner_image_uploader.rb
+++ app/uploaders/banner_image_uploader.rb
@@ -2,7 +2,9 @@ require "image_processing/mini_magick"
 
 class BannerImageUploader < Shrine
   Attacher.derivatives do |original|
-    magick = ImageProcessing::MiniMagick.source(original)
+    magick = ImageProcessing::MiniMagick
+      .source(original)
+      .convert("webp")
 
     {
       thumbnail:  magick.resize_to_fill!(240, 240, gravity: "south-west"),
