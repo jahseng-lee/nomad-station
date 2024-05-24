c1373c897e5e34fc2e7f125b1f87fc78f8581800
Jah Seng Lee <jahseng.lee@gmail.com>
2024-05-24 09:05:06 +0900

    Update comment to NOTE

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,12 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    # NOTE: we're still reviews.pluck("avg(overall)") for reviews
+    #       for each location, but fine for now.
+    #       Consider:
+    #       * lazily loading review averages; AND/OR
+    #       * a materialized view
+    #       if it gets very slow
     @pagy, @locations = pagy(
       Location.all
         .includes(:tags)
